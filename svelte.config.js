e656deef1f0cbeb8b78559813dc47a779f36f3d1
Lynn <luke87joseph@gmail.com>
2025-11-18 15:35:01 +0000

    Initialize SvelteKit

---
+++ svelte.config.js
@@ -0,0 +1,15 @@
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base: 'oskar'
+		}
+	}
+};
+
+export default config;
